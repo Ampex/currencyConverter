8309085d218e4e93ccd230b2cb1852de5ce6bf51
Błażej Kasperski <blazej.kasperski@avrio.pl>
2020-07-23 01:39:49 +0200

    Updates

---
+++ precache-manifest.cbed406c8b1457209c54a266bb890d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6253f1bfe601ec15d29864b08006fa68",
+    "url": "/currencyConverter/index.html"
+  },
+  {
+    "revision": "c83e085deadf24b6f470",
+    "url": "/currencyConverter/static/css/main.a3af3edb.chunk.css"
+  },
+  {
+    "revision": "7a220d2f3697220c23bb",
+    "url": "/currencyConverter/static/js/2.0ed81e85.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/currencyConverter/static/js/2.0ed81e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83e085deadf24b6f470",
+    "url": "/currencyConverter/static/js/main.ec0e6d12.chunk.js"
+  },
+  {
+    "revision": "fbddb8d1cc25fb63d553",
+    "url": "/currencyConverter/static/js/runtime-main.9d1ea572.js"
+  }
+]);
